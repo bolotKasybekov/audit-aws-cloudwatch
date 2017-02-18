7fedb15cf51ad1e6974cdee9d4bacb7f3c442cbd
George Moberly <george.moberly@gmail.com>
2017-02-18 05:43:11 -0800

    test inventory

--- services/config.rb
+++ services/config.rb
@@ -5,28 +5,28 @@ coreo_uni_util_variables "cloudwatch-vars" do
             ])
 end
 
-# coreo_aws_rule "cloudwatch-inventory" do
-#   action :define
-#   service :cloudwatch
-#   link "http://kb.cloudcoreo.com/mydoc-inventory.html"
-#   include_violations_in_count false
-#   display_name "Cloudwatch Inventory"
-#   description "This rule performs an inventory on all cloudwatch objects in the target AWS account."
-#   category "Inventory"
-#   suggested_action "None."
-#   level "Informational"
-#   meta_cis_id "99.999"
-#   objectives ["alarms"]
-#   audit_objects ["object.metric_alarms.alarm_name"]
-#   operators ["=~"]
-#   raise_when [//]
-#   id_map "object.metric_alarms.alarm_name"
-# end
+coreo_aws_rule "cloudwatch-inventory" do
+  action :define
+  service :cloudwatch
+  link "http://kb.cloudcoreo.com/mydoc-inventory.html"
+  include_violations_in_count false
+  display_name "Cloudwatch Inventory"
+  description "This rule performs an inventory on all cloudwatch objects in the target AWS account."
+  category "Inventory"
+  suggested_action "None."
+  level "Informational"
+  meta_cis_id "99.999"
+  objectives ["alarms"]
+  audit_objects ["object.metric_alarms.alarm_name"]
+  operators ["=~"]
+  raise_when [//]
+  id_map "object.metric_alarms.alarm_name"
+end
 
-# coreo_aws_rule_runner "advise-cloudwatch" do
-#   rules ${AUDIT_AWS_CLOUDWATCH_ALERT_LIST}
-#   action :run
-#   service :cloudwatch
-#   regions ${AUDIT_AWS_CLOUDWATCH_REGIONS}
-# end
+coreo_aws_rule_runner "advise-cloudwatch" do
+  rules ${AUDIT_AWS_CLOUDWATCH_ALERT_LIST}
+  action :run
+  service :cloudwatch
+  regions ${AUDIT_AWS_CLOUDWATCH_REGIONS}
+end
 
