6b2b1ade69e1495232927dfc2e4313dd695b7270
Michael Pautov <123prove123@gmail.com>
2017-02-22 09:22:21 +0600

    Updated JSRunner

--- services/config.rb
+++ services/config.rb
@@ -59,7 +59,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-cloudwatch" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.8.5"
+                   :version => "1.8.6"
                },
                {
                    :name => "js-yaml",
