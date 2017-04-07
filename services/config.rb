53b3c1ea6195abe7f7ec87936e3a354402152d19
Michael Pautov <michael.pautov@gmail.com>
2017-04-07 11:21:02 +0600

    Changed plandwide.table to GLOBAL::table

--- services/config.rb
+++ services/config.rb
@@ -124,7 +124,7 @@ coreo_uni_util_variables "cloudwatch-update-planwide-3" do
   variables([
                 {'COMPOSITE::coreo_aws_rule_runner.advise-cloudwatch.report' => 'COMPOSITE::coreo_uni_util_jsrunner.tags-to-notifiers-array-cloudwatch.report'},
                 {'COMPOSITE::coreo_uni_util_variables.cloudwatch-planwide.results' => 'COMPOSITE::coreo_uni_util_jsrunner.tags-to-notifiers-array-cloudwatch.JSONReport'},
-                {'COMPOSITE::coreo_uni_util_variables.cloudwatch-planwide.table' => 'COMPOSITE::coreo_uni_util_jsrunner.tags-to-notifiers-array-cloudwatch.table'}
+                {'GLOBAL::table' => 'COMPOSITE::coreo_uni_util_jsrunner.tags-to-notifiers-array-cloudwatch.table'}
             ])
 end
 
