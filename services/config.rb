e3fb0ed484497627585b098f38b1a93d1aaa3a51
Michael Pautov <michael.pautov@gmail.com>
2017-05-15 11:44:18 +0600

    Rewrote input for alert list

--- services/config.rb
+++ services/config.rb
@@ -73,7 +73,7 @@ const OWNER_TAG = "${AUDIT_AWS_CLOUDWATCH_OWNER_TAG}";
 const ALLOW_EMPTY = "${AUDIT_AWS_CLOUDWATCH_ALLOW_EMPTY}";
 const SEND_ON = "${AUDIT_AWS_CLOUDWATCH_SEND_ON}";
 
-const alertListJSON = "[${AUDIT_AWS_CLOUDWATCH_ALERT_LIST}]";
+const alertListJSON = [${AUDIT_AWS_CLOUDWATCH_ALERT_LIST}];
 const alertListArray = alertListJSON.replace(/'/g, '"');
 const ruleInputs = {};
 let userSuppression;
