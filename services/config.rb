07983c3c540604d034bce04359cb07a94a5db006
nursultan <nursultan@cloudcoure.com>
2017-06-12 14:03:04 +0600

    jsrunner-common module is updated

--- services/config.rb
+++ services/config.rb
@@ -50,7 +50,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-cloudwatch" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.9.7-beta22"
+                   :version => "1.9.7-beta29"
                },
                {
                    :name => "js-yaml",
