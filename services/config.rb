b784833b6885f8808974551c34c0e07fad8a07f4
Dmitrii Gumennikov <dmitrii@Dmitriis-MacBook-Pro.local>
2017-07-14 14:02:26 +0600

    updated jsrunner to the stable version

--- services/config.rb
+++ services/config.rb
@@ -51,7 +51,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-cloudwatch" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.10.7-18"
+                   :version => "1.10.7-19"
                },
                {
                    :name => "js-yaml",
