aad0e92515a96d806c5a09e7531779365d04cbbc
omurbek.jk@gmail.com <omurbek.jk@gmail.com>
2017-07-26 11:56:20 +0600

    added teamName to email reports

--- services/config.rb
+++ services/config.rb
@@ -60,6 +60,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-cloudwatch" do
            ])
   json_input '{ "compositeName":"PLAN::stack_name",
                 "planName":"PLAN::name",
+                "teamName":"PLAN::team_name",
                 "cloudAccountName": "PLAN::cloud_account_name",
                 "violations": COMPOSITE::coreo_aws_rule_runner.advise-cloudwatch.report}'
   function <<-EOH
@@ -68,6 +69,7 @@ const compositeName = json_input.compositeName;
 const planName = json_input.planName;
 const cloudAccount = json_input.cloudAccountName;
 const cloudObjects = json_input.violations;
+const teamName = json_input.teamName;
 
 const NO_OWNER_EMAIL = "${AUDIT_AWS_CLOUDWATCH_ALERT_RECIPIENT}";
 const OWNER_TAG = "${AUDIT_AWS_CLOUDWATCH_OWNER_TAG}";
@@ -121,7 +123,7 @@ setTable();
 const argForConfig = {
     NO_OWNER_EMAIL, cloudObjects, userSuppression, OWNER_TAG,
     userSchemes, alertListArray, ruleInputs, ALLOW_EMPTY,
-    SEND_ON, cloudAccount, compositeName, planName, htmlReportSubject
+    SEND_ON, cloudAccount, compositeName, planName, htmlReportSubject, teamName
 }
 
 
@@ -130,6 +132,7 @@ function createConfig(argForConfig) {
         compositeName: argForConfig.compositeName,
         htmlReportSubject: argForConfig.htmlReportSubject,
         planName: argForConfig.planName,
+        teamName: argForConfig.teamName,
         violations: argForConfig.cloudObjects,
         userSchemes: argForConfig.userSchemes,
         userSuppression: argForConfig.userSuppression,
