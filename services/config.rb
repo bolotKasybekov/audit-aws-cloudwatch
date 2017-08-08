c9834c40ef27006afead4316a0c9c899e9e0a578
omurbek.jk@gmail.com <omurbek.jk@gmail.com>
2017-08-08 09:24:39 +0600

    CON-617-emails not going out for inventory reports

--- services/config.rb
+++ services/config.rb
@@ -51,7 +51,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-cloudwatch" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.10.7-beta60"
+                   :version => "1.10.7-beta63"
                },
                {
                    :name => "js-yaml",
@@ -192,7 +192,7 @@ function setTextRollup() {
         }
     });
 
-    textRollup += 'Total Number of Violating Cloud Objects: ' + numberOfViolations + "\\n";
+    textRollup += 'Total Number of matching Cloud Objects: ' + numberOfViolations + "\\n";
     textRollup += 'Rollup' + "\\n";
     textRollup += emailText;
 
