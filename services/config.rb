e291d656ef3bcb0cc04882e533cbab48a1742594
omurbek.jk@gmail.com <omurbek.jk@gmail.com>
2017-08-18 11:36:32 +0600

    PLA-4508-fixed name adjusment

--- services/config.rb
+++ services/config.rb
@@ -227,7 +227,7 @@ COMPOSITE::coreo_uni_util_jsrunner.tags-rollup-cloudwatch.return
 end
 
 coreo_aws_s3_policy "cloudcoreo-audit-aws-cloudwatch-policy" do
-  action((("${S3_BUCKET_NAME}".length > 0) ) ? :create : :nothing)
+  action((("${AUDIT_AWS_CLOUDWATCH_S3_NOTIFICATION_BUCKET_NAME}".length > 0) ) ? :create : :nothing)
   policy_document <<-EOF
 {
 "Version": "2012-10-17",
@@ -240,8 +240,8 @@ coreo_aws_s3_policy "cloudcoreo-audit-aws-cloudwatch-policy" do
 ,
 "Action": "s3:*",
 "Resource": [
-"arn:aws:s3:::${S3_BUCKET_NAME}/*",
-"arn:aws:s3:::${S3_BUCKET_NAME}"
+"arn:aws:s3:::${AUDIT_AWS_CLOUDWATCH_S3_NOTIFICATION_BUCKET_NAME}/*",
+"arn:aws:s3:::${AUDIT_AWS_CLOUDWATCH_S3_NOTIFICATION_BUCKET_NAME}"
 ]
 }
 ]
@@ -250,19 +250,19 @@ coreo_aws_s3_policy "cloudcoreo-audit-aws-cloudwatch-policy" do
 end
 
 coreo_aws_s3_bucket "cloudcoreo-audit-aws-cloudwatch" do
-  action :create
+  action((("${AUDIT_AWS_CLOUDWATCH_S3_NOTIFICATION_BUCKET_NAME}".length > 0) ) ? :create : :nothing)
   bucket_policies ["cloudcoreo-audit-aws-cloudwatch-policy"]
   region "us-east-1"
 end
 
 coreo_uni_util_notify "cloudcoreo-audit-aws-cloudwatch-s3" do
-  action((("${S3_BUCKET_NAME}".length > 0) ) ? :notify : :nothing)
+  action((("${AUDIT_AWS_CLOUDWATCH_S3_NOTIFICATION_BUCKET_NAME}".length > 0) ) ? :notify : :nothing)
   type 's3'
   allow_empty true
   payload 'COMPOSITE::coreo_uni_util_jsrunner.tags-to-notifiers-array-cloudwatch.report'
   endpoint ({
       object_name: 'aws-cloudwatch-json',
-      bucket_name: '${S3_BUCKET_NAME}',
+      bucket_name: '${AUDIT_AWS_CLOUDWATCH_S3_NOTIFICATION_BUCKET_NAME}',
       folder: 'cloudwatch/PLAN::name',
       properties: {}
   })
